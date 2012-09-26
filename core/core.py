de74d54369d207950d0a870c972c51e9c7a5c267
Ashvin Goel <ashvin@ug149.eecg>
2012-09-26 00:58:47 -0400

    adding verbose option to os161-tester

--- core/core.py
+++ core/core.py
@@ -2,11 +2,17 @@
 
 import pexpect
 import sys
+import os
 
 class TestUnit:
 	#Implicit assumptions, sys161 is in path
 	def __init__(self, path_to_kernel, message):
 		global kernel
+                global verbose
+                try:
+                        verbose = os.environ['OS161_TESTER_VERBOSE']
+                except KeyError:
+                        verbose = 0
 		path = 'sys161 ' + str(path_to_kernel)
 		kernel = pexpect.spawn(path)
 		#kernel.logfile = sys.stdout
@@ -18,6 +24,8 @@ class TestUnit:
 		#The fun bit is, we need to send the command character by
 		#character to the simulator, otherwise we are going to have
 		#a lot of fun ;-)
+                if verbose != 0:
+                        print "SENDING: " + cmd
 		cmd_char = list(cmd)
 		for i in cmd_char:
 			kernel.send(i)
@@ -26,7 +34,8 @@ class TestUnit:
 
 	def basic_read_test(self, result):
 		try:
-                        # print "running expect on " + result
+                        if verbose != 0:
+                                print "EXPECTING: " + result
 			kernel.expect(result)
 		except pexpect.TIMEOUT, e:
 			return False
