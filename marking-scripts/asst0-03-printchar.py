20012c0e9d095d8eb7f1cfdec919cefb28a5b25f
Dhaval Giani <dhaval@eecg.toronto.edu>
2012-10-09 16:02:09 -0400

    Add tests for printchar with the help of a custom printchar
    Signed-off-by: Dhaval Giani <dhaval@eecg.toronto.edu>

--- marking-scripts/asst0-03-printchar.py
+++ marking-scripts/asst0-03-printchar.py
@@ -7,10 +7,12 @@ def testPrintChar(kernel_name):
 	global test
 	test = core.TestUnit(kernel_name, "Testing printchar")
 	# check = 'H*e*l*l*o*w*o*r*l*d*\!*H*e*l*l*o*p*r*i*n*t*f*\!*'
-	check = 'H.*e.*l.*l.*o.*w.*o.*r.*l.*d.*\!.*H.*e.*l.*l.*o.*p.*r.*i.*n.*t.*f.*\!'
+	# check = 'H.*e.*l.*l.*o.*w.*o.*r.*l.*d.*\!.*H.*e.*l.*l.*o.*p.*r.*i.*n.*t.*f.*\!'
 	#check = 'H*e*l*l*o*p*r*i*n*t*f*\!'
 	#check = 'H**e**l**l**o**p**r**i**n**t**f**\!'
-	test.send_command("p /testbin/printchar")
+	printchar_check = '$.#.@.$.#.@.'
+	printf_check = 'S.Y.S.1.6.1.O.S.T.E.S.T.E.R.F.O.R.P.R.I.N.T.F'
+	test.send_command("p /testbin/test_printchar")
 	test.look_for_and_print_result(check)
 
 def main():
