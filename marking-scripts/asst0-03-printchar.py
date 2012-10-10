570739b6e51274d03c0b3e2e4f3852e3da4b9e13
Dhaval Giani <dhaval@eecg.toronto.edu>
2012-10-10 13:50:32 -0400

    escape special symbols
    Signed-off-by: Dhaval Giani <dhaval@eecg.toronto.edu>

--- marking-scripts/asst0-03-printchar.py
+++ marking-scripts/asst0-03-printchar.py
@@ -10,7 +10,7 @@ def testPrintChar(kernel_name):
 	# check = 'H.*e.*l.*l.*o.*w.*o.*r.*l.*d.*\!.*H.*e.*l.*l.*o.*p.*r.*i.*n.*t.*f.*\!'
 	#check = 'H*e*l*l*o*p*r*i*n*t*f*\!'
 	#check = 'H**e**l**l**o**p**r**i**n**t**f**\!'
-	printchar_check = '$.#.@.$.#.@.'
+	printchar_check = '\$.\#.\@.\$.\#.\@.'
 	printf_check = 'S.Y.S.1.6.1.O.S.T.E.S.T.E.R.F.O.R.P.R.I.N.T.F'
 	test.send_command("p /testbin/test_printchar")
 	mark = test.look_for_and_return_mark(printchar_check+printf_check, 20)
