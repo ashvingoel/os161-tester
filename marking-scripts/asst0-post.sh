431bc151c08acaf9c5907e350dfe0974949126ed
Dhaval Giani <dhaval.giani@gmail.com>
2012-10-15 19:28:59 -0400

    Get the asst0 post install scripts
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- marking-scripts/asst0-post.sh
+++ marking-scripts/asst0-post.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# copy our own printchar tester and build/install it
-if [  -G "$TEST_PATH/marking-scripts" ]; then
-    /bin/cp -r $TEST_PATH/marking-scripts/asst0-code/test_printchar ../../../testbin/
-    cd ../../../testbin/test_printchar && make -s && make -s install
-fi
+pushd .
 
+cd $BUILD_PATH../asst0-end/
+
+# copy our own printchar tester and build/install it
+/bin/cp -r $TEST_PATH/marking-scripts/asst0-code/test_printchar testbin/
+cd testbin/test_printchar && make -s && make -s install
+
+popd
