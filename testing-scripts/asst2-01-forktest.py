9052458766b5cfbefc21c6c5b2de5abc77e91154
Ashvin Goel <ashvin.goel@gmail.com>
2012-11-06 10:29:10 -0500

    updated forktest

--- testing-scripts/asst2-01-forktest.py
+++ testing-scripts/asst2-01-forktest.py
@@ -6,9 +6,29 @@ import sys
 def testForkTest(kernel_name):
 	global test
 	test = core.TestUnit(kernel_name, "Testing forktest")
-	check = '001111222222223333333333333333'
 	test.send_command("p /testbin/forktest")
-	test.look_for_and_print_result(check, 20)
+
+        # when we fix forktest, this is all we need to look for
+	# check = '001111222222223333333333333333'
+        # test.look_for_and_print_result(check, 20)
+
+        out = test.look_for("([0123]{30,30})$")
+        if out < 0:
+            test.print_result(0, 20)
+        else:
+            output = test.kernel.match.group(0)
+            output_array = [0] * 4
+            for i in range(len(output)):
+                index = int(output[i])
+                output_array[index] = output_array[index] + 1
+            if (output_array[0] == 2) and \
+                    (output_array[1] == 4) and \
+                    (output_array[2] == 8) and \
+                    (output_array[3] == 16):
+                test.print_result(20, 20)
+            else:
+                print "Output didn't match"
+                test.print_result(0, 20)
 
 def main():
 	path = str(sys.argv[1])
