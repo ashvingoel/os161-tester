37aefb8a59bbbc8541978088aba6e05e72dda02f
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-07 18:42:34 -0500

    Get the Assignment 2 post script setup
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- marking-scripts/asst2-post.sh
+++ marking-scripts/asst2-post.sh
@@ -2,10 +2,13 @@
 
 pushd .
 
-cd $BUILD_PATH../asst2-end/
+cd ${BUILD_PATH}../asst2-end/
 
 # copy our own printchar tester and build/install it
-/bin/cp -r $TEST_PATH/marking-scripts/asst0-code/test_printchar testbin/
-cd testbin/test_printchar && make -s && make -s install
-
+for i in `ls ${TEST_PATH}/marking-scripts/asst2-code/`
+do
+	pushd .
+	/bin/cp -r $TEST_PATH/marking-scripts/asst0-code/$i testbin/
+	cd testbin/$i && make -s && make -s install
+	popd
 popd
