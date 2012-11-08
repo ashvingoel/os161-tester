ecfb1d6c6837cb0c8bdbe5c2db1ac56d9f2fb844
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-07 21:38:48 -0500

    Fix up a bug somewhere
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- marking-scripts/asst2-06-waitexit4.py
+++ marking-scripts/asst2-06-waitexit4.py
@@ -5,10 +5,10 @@ import sys
 
 def testPrintChar(kernel_name):
 	global test
-	test = core.TestUnit(kernel_name, "Testing use of wait and exit I")
-	check = 'wekp'
-	test.send_command("p /testbin/waitexit1")
-	test.look_for_and_print_result(check, 6)
+	test = core.TestUnit(kernel_name, "Testing use of wait and exit IV")
+	check = 'apc'
+	test.send_command("p /testbin/waitexit4")
+	test.look_for_and_print_result(check, 5)
 
 def main():
 	path = str(sys.argv[1])
