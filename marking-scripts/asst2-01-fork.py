a2a52e917c60d488f283b76aa5bdcc94d892a421
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-07 21:10:37 -0500

    More test cases
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- marking-scripts/asst2-01-fork.py
+++ marking-scripts/asst2-01-fork.py
@@ -3,36 +3,16 @@
 import core
 import sys
 
-def testForkTest(kernel_name):
+def testPrintChar(kernel_name):
 	global test
 	test = core.TestUnit(kernel_name, "Testing fork")
+	check = '00aa111b1bbb2222c22c2cc2cccc'
 	test.send_command("p /testbin/fork")
-
-        # when we fix forktest, this is all we need to look for
-	# check = '001111222222223333333333333333'
-        # test.look_for_and_print_result(check, 20)
-
-        out = test.look_for("([0123]{30,30})$")
-        if out < 0:
-            test.print_result(0, 20)
-        else:
-            output = test.kernel.match.group(0)
-            output_array = [0] * 4
-            for i in range(len(output)):
-                index = int(output[i])
-                output_array[index] = output_array[index] + 1
-            if (output_array[0] == 2) and \
-                    (output_array[1] == 4) and \
-                    (output_array[2] == 8) and \
-                    (output_array[3] == 16):
-                test.print_result(20, 20)
-            else:
-                print "Output didn't match"
-                test.print_result(0, 20)
+	test.look_for_and_print_result(check, 8)
 
 def main():
 	path = str(sys.argv[1])
-	testForkTest(path)
+	testPrintChar(path)
 
 
 
--- marking-scripts/asst2-02-multiply.py
+++ marking-scripts/asst2-02-multiply.py
@@ -5,10 +5,10 @@ import sys
 
 def testPrintChar(kernel_name):
 	global test
-	test = core.TestUnit(kernel_name, "Testing fork")
-	check = '00aa111b1bbb2222c22c2cc2cccc'
-	test.send_command("p /testbin/fork")
-	test.look_for_and_print_result(check, 12)
+	test = core.TestUnit(kernel_name, "Testing multiply")
+	check = '15'
+	test.send_command("p /testbin/multiply 3 5")
+	test.look_for_and_print_result(check, 5)
 
 def main():
 	path = str(sys.argv[1])
