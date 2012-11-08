d1b1c6e8876796b585deb60ae70f9732107ed504
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-07 21:02:59 -0500

    Test using testbin/fork
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

---
+++ marking-scripts/asst2-01-forktest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import core
+import sys
+
+def testPrintChar(kernel_name):
+	global test
+	test = core.TestUnit(kernel_name, "Testing fork")
+	check = '00aa111b1bbb2222c22c2cc2cccc'
+	test.send_command("p /testbin/fork")
+	test.look_for_and_print_result(check, 12)
+
+def main():
+	path = str(sys.argv[1])
+	testPrintChar(path)
+
+
+
+if __name__ == "__main__":
+	main()
