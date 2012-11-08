8369de4d1f85b52f5a251a6f50a6fcf2b6ca6e2d
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-07 20:38:12 -0500

    Debug
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- marking-scripts/asst2-post.sh
+++ marking-scripts/asst2-post.sh
@@ -8,7 +8,7 @@ echo "TEST_PATH is $TEST_PATH"
 
 pushd .
 
-cd ${BUILD_PATH}/asst2-end/
+cd ${BUILD_PATH}/../asst2-end/
 
 # copy our own printchar tester and build/install it
 for i in `ls ${TEST_PATH}/marking-scripts/asst2-code/`
