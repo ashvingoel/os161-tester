67e6e87de1e512c30002d5eabee329bc72a91727
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-12 18:37:25 -0500

    Make the script more verbose
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- bin/get-diff.sh
+++ bin/get-diff.sh
@@ -5,5 +5,6 @@ END_GRP=39
 for i in `seq -f "%03g" ${START_GRP} ${END_GRP}`
 do
 	SVN_REP=${ECE_SVN}/os-${i}/svn
-	svn diff ${SVN_REP}/tags/asst2-start ${SVN_REP}/tags/asst2-end > os161-asst-$i.patch
+	echo "Downloading patch for os-${i}"
+	svn diff ${SVN_REP}/tags/asst2-start ${SVN_REP}/tags/asst2-end > os161-asst-${i}.patch
 done
