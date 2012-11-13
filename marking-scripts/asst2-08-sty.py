7f72efbc3ee44c53e7fd8ed60dcf3838828cffd4
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-12 23:01:47 -0500

    Make the sty test more robust
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- marking-scripts/asst2-08-sty.py
+++ marking-scripts/asst2-08-sty.py
@@ -6,12 +6,17 @@ import sys
 def testPrintChar(kernel_name):
 	global test
 	test = core.TestUnit(kernel_name, "Checking the OS does not crash when OOM")
+	fail_check = 'No such file or directory'
 	check = 'OS\/161 kernel \[\? for menu\]\: '
 	for i in range(1,10):
 		test.send_command("p /testbin/sty")
 		ret = test.look_for_and_print_result(check, 0)
 
 	test.send_command("p /testbin/sty")
+	#This is a hacky check to see we actually built sty
+	fail = test.look_for(fail_check)
+	if fail >= 0:
+		test.print_result(0, 6)
 	test.look_for_and_print_result(check, 6)
 
 def main():
