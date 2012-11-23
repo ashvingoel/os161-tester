92a4c50d2213ebf9e4571ebe7403709ee2ff8a07
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-23 17:55:12 -0500

    Asst3 Matmult test
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

---
+++ testing-scripts/asst3-01-matmult.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/python
+
+import core
+import sys
+
+def testPrintChar(kernel_name):
+	global test
+	test = core.TestUnit(kernel_name, "Testing matmult")
+	check = 'answer is: 82313733 \(should be 8772192\)'
+	test.send_command("p /testbin/matmult")
+	test.look_for_and_print_result(check, 5)
+
+def main():
+	path = str(sys.argv[1])
+	testPrintChar(path)
+
+
+
+if __name__ == "__main__":
+	main()
