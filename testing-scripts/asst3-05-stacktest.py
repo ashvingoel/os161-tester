29e9bfcf11abe4e01cd5264000c7c83d53da141f
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-23 22:57:57 -0500

    Debug Code
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- testing-scripts/asst3-05-stacktest.py
+++ testing-scripts/asst3-05-stacktest.py
@@ -10,6 +10,7 @@ def testPrintChar(kernel_name):
 	test.send_command("p /testbin/stacktest")
 	f = open('stacktest.in', 'r')
 	for check in f.readlines():
+		print check
 		test.look_for_and_print_result(check, 1)
 	f.close()
 
