07f0b524711f82966f2b677fd7b00ff1ccc39263
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-23 21:21:46 -0500

    Test an ugly hack
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- testing-scripts/asst3-03-triplemult.py
+++ testing-scripts/asst3-03-triplemult.py
@@ -7,13 +7,23 @@ def testPrintChar(kernel_name):
 	global test
 	test = core.TestUnit(kernel_name, "Testing triplemat")
 	test.set_timeout(120)
-	check = 'answer is: 8772192 (should be 8772192)'
+	check1 = 'answer is'
+	check2 = '8772192'
+	check3 = 'should be 8772192'
 	test.send_command("p /testbin/triplemat")
 	for i in range(3):
-		test.look_for_and_print_result(check, 5)
+		ret = test.look_for(check1)
+		if ret >= 0:
+			ret = test.look_for(check2)
+			if ret >=0:
+				test.look_for_and_print_result(check3, 5)
+			else
+				test.print_result(0,5)
+		else
+			test.print_result(0,5)
 	#We should have finished everything by now, so let's just reduce the timeout
 	test.set_timeout(10)
-	check = 'testbin/triplemat: Congratulations! You passed.'
+	check = 'Congratulations! You passed'
 	test.look_for_and_print_result(check, 5)
 
 def main():
