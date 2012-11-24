61d0e534a21f6826913501242e3b876610da4582
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-23 21:02:04 -0500

    Fixup a check
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- testing-scripts/asst3-01-matmult.py
+++ testing-scripts/asst3-01-matmult.py
@@ -6,8 +6,8 @@ import sys
 def testPrintChar(kernel_name):
 	global test
 	test = core.TestUnit(kernel_name, "Testing matmult")
-	test.set_timeout(600)
-	check = 'answer is: 82313733 \(should be 8772192\)'
+	test.set_timeout(60)
+	check = 'answer is: 8772192\(should be 8772192\)'
 	test.send_command("p /testbin/matmult")
 	test.look_for_and_print_result(check, 5)
 
--- testing-scripts/asst3-03-triplemult.py
+++ testing-scripts/asst3-03-triplemult.py
@@ -6,11 +6,13 @@ import sys
 def testPrintChar(kernel_name):
 	global test
 	test = core.TestUnit(kernel_name, "Testing triplemat")
-	test.set_timeout(600)
-	check = 'answer is: 82313733 \(should be 8772192\)'
+	test.set_timeout(120)
+	check = 'answer is: 8772192 (should be 8772192)'
 	test.send_command("p /testbin/triplemat")
 	for i in range(3):
 		test.look_for_and_print_result(check, 5)
+	#We should have finished everything by now, so let's just reduce the timeout
+	test.set_timeout(10)
 	check = 'testbin/triplemat: Congratulations! You passed.'
 	test.look_for_and_print_result(check, 5)
 
