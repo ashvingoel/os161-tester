4153632187dbc5b640329024cc25e364fd61193f
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-23 23:25:22 -0500

    Fix the correct paths
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- testing-scripts/asst3-05-stacktest.py
+++ testing-scripts/asst3-05-stacktest.py
@@ -6,11 +6,11 @@ import os
 
 def testPrintChar(kernel_name):
 	global test
-	stackfile = os.environ['TESTER_BIN_PATH']
+	stackfile = os.environ['TEST_PATH']
 	test = core.TestUnit(kernel_name, "Testing stacktest")
 	test.set_timeout(10)
 	test.send_command("p /testbin/stacktest")
-	f = open(stackfile + '/../stacktest.in', 'r')
+	f = open(stackfile + '/testing-files/stacktest.in', 'r')
 	for check in f.readlines():
 		test.look_for_and_print_result(check.strip(), 1)
 	f.close()
