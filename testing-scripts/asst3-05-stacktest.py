f77baa15e0b85f287da4c70717d9619679ab01cd
Dhaval Giani <dhaval.giani@gmail.com>
2012-11-23 22:13:18 -0500

    Add a stacktest to get the log of what comes out.
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

---
+++ testing-scripts/asst3-05-stacktest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import core
+import sys
+
+def testPrintChar(kernel_name):
+	global test
+	test = core.TestUnit(kernel_name, "Testing palin")
+	test.set_timeout(60)
+	check = 'IS a palindrome'
+	test.send_command("p /testbin/palin")
+	test.look_for_and_print_result(check, 5)
+
+def main():
+	path = str(sys.argv[1])
+	testPrintChar(path)
+
+
+if __name__ == "__main__":
+	main()
