8d15f6b26c33d99ba05176b6ad801b72ea51d979
Ashvin Goel <ashvin.goel@gmail.com>
2013-11-27 05:58:54 -0500

    updated crasho tester script

--- scripts/asst3-11-crasho.py
+++ scripts/asst3-11-crasho.py
@@ -7,7 +7,7 @@ def main():
     test = core.TestUnit("crasho")
     test.runprogram("/testbin/crash", "o")
     test.set_timeout(540)
-    test.look_for_and_print_result('\nFatal user mode trap 3', 10)
+    test.look_for_and_print_result('\nFatal user mode trap (3|4)', 10)
 
 if __name__ == "__main__":
 	main()
